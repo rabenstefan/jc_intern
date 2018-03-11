f8929f3b9d6a560e739daff039c4e3ee57f5cffe
Niels Ranosch <niels@ranosch.de>
2018-03-11 15:44:52 +0100

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @if(View::hasSection('title'))
-        <title>@yield('title') - Jazzchor der Uni Bonn</title>
-        @else <title>Interner Bereich - Jazzchor der Uni Bonn</title>
-    @endif
+    <title>@yield('title', 'Interner Bereich') - Jazzchor der Uni Bonn</title>
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
