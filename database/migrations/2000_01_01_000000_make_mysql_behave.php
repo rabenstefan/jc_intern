1945e6b84aef9cb25d0d8738b7fcebcca3a0f750
Niels Ranosch <niels@ranosch.de>
2018-03-11 15:10:06 +0100

    MySQL and Composer/Laravel compatibility

--- database/migrations/2000_01_01_000000_make_mysql_behave.php
+++ database/migrations/2000_01_01_000000_make_mysql_behave.php
@@ -21,6 +21,6 @@ class MakeMysqlBehave extends Migration
      */
     public function down()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
     }
 }
