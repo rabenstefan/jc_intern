90c985a11fdfba6d3969a8a353c404a505e25ef0
Niels Ranosch <niels@ranosch.de>
2018-03-12 13:02:57 +0100

    improved functionality and code of calendar filters

--- resources/views/date/settings_bar.blade.php
+++ resources/views/date/settings_bar.blade.php
@@ -34,75 +34,27 @@ $view_types = ['calendar', 'list']; ?>
 
     <script type="text/javascript">
         $(document).ready(function () {
-
-            function hideFilterByType(singular, plural) {
-                @if ($view_type === 'list')
-                $('.event.event-' + singular).parent('.row.list-item').hide();
-                @elseif ($view_type === 'calendar')
-                $('.fc-event.event-' + singular).parent('.fc-event-container').hide();
-                @endif
-                $('#toggle-' + plural).removeClass('btn-pressed');
-                Cookies.set('EventFilterActive_'+singular, false, { expires: 1/12 });
-            }
-
-            function showFilterByType(singular, plural) {
-                @if ($view_type === 'list')
-                $('.event.event-' + singular).parent('.row.list-item').show();
-                @elseif ($view_type === 'calendar')
-                $('.fc-event.event-' + singular).parent('.fc-event-container').show();
-                @endif
-                $('#toggle-' + plural).addClass('btn-pressed');
-                Cookies.set('EventFilterActive_'+singular, true, { expires: 1/12 });
-            }
-
-            function showAllFilters() {
-                @foreach($filters as $singular)
-                <?php $plural = str_plural($singular); ?>
-                showFilterByType('{{$singular}}', '{{$plural}}');
-                @endforeach
-            }
-
-            function toggleFilterByType(singular, plural) {
-                // We check hasClass and not the Cookie, to not confuse people who use multiple tabs/windows.
-                if ($('#toggle-' + plural).hasClass('btn-pressed')) {
-                    // Previously shown. Hide!
-                    hideFilterByType(singular, plural);
-                } else {
-                    // Hidden. Show!
-                    showFilterByType(singular, plural);
-                }
-            }
-
-
-            @foreach($filters as $singular)
-                <?php $plural = str_plural($singular); ?>
-
-                if (!(undefined === Cookies.get('EventFilterActive_{{$singular}}')) && ('false' === Cookies.get('EventFilterActive_{{$singular}}'))) {
-                    hideFilterByType('{{$singular}}', '{{$plural}}');
-                }
-
-                $('#toggle-{{$plural}}').click(function () {
-                    toggleFilterByType('{{$singular}}', '{{$plural}}');
-                });
+            @foreach($filters as $filter)
+                dateFilters.availableFilters.push({'singular': '{{ $filter }}', 'plural': '{{ str_plural($filter) }}'});
             @endforeach
 
-            @if(null !== $override_filters)
-            // showOnly-type arguments have been passed to PHP. We will now parse them
-            showAllFilters();
-            <?php $override_filters = array_intersect($filters, $override_filters); ?> {{-- Because never trust the client! --}}
+            dateFilters.prepareButtons();
+
+            @if(null === $override_filters)
+                dateFilters.applyAllFromCookie();
+            @else
+                // Some options to override filters have been passed as GET-parameters. We will now try to parse them to javascript
+                dateFilters.showAll();
+                <?php $override_filters = array_intersect($filters, $override_filters); ?> {{-- Because never trust the client! --}}
                 @if(count($override_filters) === 0)
                     $.notify('{!! trans('date.filters_invalid') !!}', {className: 'warning', autoHideDelay: 5000});
                 @else @foreach(array_diff($filters, $override_filters) as $singular)
                     {{-- Sinve $override_filters was modified by array_intersect above, we now have
-                      all instances of filters we want to hide --}}
+                        all instances of filters we want to hide --}}
                     <?php $plural = str_plural($singular); ?>
-                    hideFilterByType('{{$singular}}', '{{$plural}}');
+                    dateFilters.hideByType('{{$singular}}', '{{$plural}}');
                 @endforeach @endif
             @endif
-
-            $('#toggle-all').click(function () {
-                showAllFilters();
-            });
         });
     </script>
 @endsection
