f85cae54d1c21b91ef8f831aa66d71c03874f215
Niels Ranosch <niels@ranosch.de>
2018-03-31 16:17:12 +0200

    trying to change wording a bit

--- resources/views/date/calendar_sync.blade.php
+++ resources/views/date/calendar_sync.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    {{trans('date.calendar_sync_explanation')}}
+
     <ul class="calendar_sync">
     @foreach(power_set($date_types) as $subset)
-        <li>
-            {{ implode(' ' . trans('date.and') . ' ', array_map(function($date_type) {return trans('date.' . $date_type);}, $subset)) }}
+        <li>{{ implode(' ' . trans('date.and') . ' ', array_map(function($date_type) {return trans('date.' . $date_type);}, $subset)) }}
             <ul>
-                <li><a href="{{ route('dates.renderIcal', ['show_types' => $subset]) }}">{{ trans('date.http') }}</a></li>
-                <li><a href="webcal://{{ \Config::get('app.domain') }}{{ route('dates.renderIcal', ['show_types' => $subset], false) }}">{{ trans('date.webcal') }}</a></li>
+                <li>Automatische Einrichtung über <a href="webcal://{{ \Config::get('app.domain') }}{{ route('dates.renderIcal', ['show_types' => $subset], false) }}">{{ trans('date.webcal') }}</a>
+                <li>Manuelle Einrichtung über HTTP: {{ route('dates.renderIcal', ['show_types' => $subset]) }}</li>
+                <!-- TODO: Add GCal synced IDs -->
             </ul>
         </li>
     @endforeach
     </ul>
+    {{trans('date.calendar_sync_explanation')}}
 @endsection
