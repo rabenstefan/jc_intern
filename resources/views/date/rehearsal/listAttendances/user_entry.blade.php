103475aa8ac3ada9f6588e706c9f4ae85b87270a
Niels Ranosch <niels@ranosch.de>
2018-04-02 16:10:43 +0200

    show comments for rehearasals

--- resources/views/date/rehearsal/listAttendances/user_entry.blade.php
+++ resources/views/date/rehearsal/listAttendances/user_entry.blade.php
@@ -1,9 +1,13 @@
-
-<div class="col-xs-6 col-sm-3 col-lg-1 names">
+<?php $comment = $currentRehearsal->hasCommented($user) ? $currentRehearsal->getComment($user) : ''; ?>
+<?php $mark = $currentRehearsal->hasCommented($user) ? '<i class="far fa-comment"></i>' : ''; ?>
+<div class="col-xs-6 col-sm-3 col-lg-1 names" title="{{ $comment }}">
     {{ $user->first_name . ' ' . $user->last_name }}
+    <span class="pull-right">
     @if($user->excusedRehearsal($currentRehearsal->id))
-        <span class="pull-right"><i class="fa fa-bed" title="{{ trans('date.excused') }}"></i></span>
+        <i class="fa fa-bed" title="{{ trans('date.excused') }}"></i>
     @endif
+        {!! $mark !!}
+    </span>
 </div>
 
 <div class="col-xs-6 col-sm-3 col-lg-2 sliders">
