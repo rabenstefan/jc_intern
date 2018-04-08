a177553aad10ae8bb87897c6f7d245b87d727e83
Niels Ranosch <niels@ranosch.de>
2018-04-08 18:18:27 +0200

    I am sorry google, but please add this as an actual feature with proper documentation so we dont have to hack our way through deprecated code that happens to be partly accessible

--- resources/views/date/calendar_sync.blade.php
+++ resources/views/date/calendar_sync.blade.php
@@ -7,9 +7,8 @@
         <li>{{ implode(' ' . trans('date.and') . ' ', array_map(function($date_type) {return trans('date.' . $date_type);}, $subset)) }}
             <ul>
                 <li>{{ trans('date.subscribe_automatically') }}: <a href="webcal://{{ \Config::get('app.domain') }}{{ route('dates.renderIcal', ['show_types' => $subset], false) }}" target="_blank">{{ trans('date.webcal') }}</a>
-                {{-- Automatically adding an iCal to Google Calendar is not officially supported. HTTPS-URLs dont work at all  --}}
-                <li>{{ trans('date.subscribe_automatically') }}: <a href="{{'https://calendar.google.com/calendar/r/settings/addbyurl?cpub=false&cid='}}@urlescape('http://' . \Config::get('app.domain') . route('dates.renderIcal', ['show_types' => $subset], false))" target="_blank">{{trans('date.gcal')}}</a></li>
-
+                {{-- Automatically adding an iCal to Google Calendar is not officially supported. HTTPS-URLs dont work at all, and once an URL was used, it cannot be used by a different Google Account (hence the id and random-string-bullcrap). We are probably only achieving this by accidentally running some deprecated code on Google's server --}}
+                <li>{{ trans('date.subscribe_automatically') }}: <a href="{{'https://calendar.google.com/calendar/r/settings/addbyurl?cpub=false&cid='}}@urlescape('http://' . \Config::get('app.domain') . route('dates.renderIcal', ['id' => Auth::getUser()->id, 'key' => str_random(3), 'show_types' => $subset], false))" target="_blank">{{trans('date.gcal')}}</a></li>
             </ul>
         </li>
     @endforeach
