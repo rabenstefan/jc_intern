b187cf563bc27ae379c5926227fd360cb1e1b2f8
Niels Ranosch <niels@ranosch.de>
2018-10-21 23:27:24 +0200

    Moved Calendar Sync Authentication to middleware.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,15 +4,15 @@ Route::group(['middleware' => 'web'], function () {
     // Include authentication routes
     Auth::routes(['register' => false, 'verify' => false]);
 
+    Route::group(['middleware' => 'auth.calendar'], function() {
+        Route::get('render_ical', 'DateController@renderIcal')->name('dates.renderIcal');
 
-    // The splash screen.
-    Route::get('/', 'HomeController@index')->name('index');
-
-    // Rendering calendars requires a different kind of middleware which is implicit for now.
-    Route::get('render_ical', 'DateController@renderIcal')->name('dates.renderIcal');
-
+    });
 
     Route::group(['middleware' => 'auth'], function() {
+        // The splash screen.
+        Route::get('/', 'HomeController@index')->name('index');
+
         Route::post('users/{user}/updateSemester', 'UserController@updateSemester')->name('users.updateSemester');
 
         // All other routes need a valid semester from the user.
