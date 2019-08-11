87b30e3031d4f5c9277382b6b2f9c69dc17e2e35
Maximilian Weiß <maximilian.weiss@gmail.com>
2019-08-11 10:39:31 +0200

    Changed icons of excuse-list.

--- resources/views/date/rehearsal/listAttendances/excuse_user_entry.blade.php
+++ resources/views/date/rehearsal/listAttendances/excuse_user_entry.blade.php
@@ -11,9 +11,9 @@
     <span class="slider-2d" data-function="excuseMissing" data-change-url="{{ route('attendances.changeOwnAttendance', ['events_name' => 'rehearsals','event_id' => $currentRehearsal->id, 'user_id' => $user->id, 'shorthand' => 'excuse']) }}">
         <input type="checkbox" {!! $user->excusedRehearsal($currentRehearsal->id) ? 'checked="checked"' : '' !!} id="slider-attending-{{ $user->id }}">
         <label for="slider-attending-{{ $user->id }}">
-            <span class="slider slider-attendance"></span>
-            <i class="fa fa-times" title="{{ trans('date.not_excused') }}"></i>
-            <i class="fa fa-check" title="{{ trans('date.excused') }}"></i>
+            <span class="slider slider-excuse"></span>
+            <i class="fa fa-question" title="{{ trans('date.not_excused') }}"></i>
+            <i class="fa fa-info" title="{{ trans('date.excused') }}"></i>
         </label>
     </span>
 </div>
